e3e4775fb858315f1471aedb489d09072d176ef2
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-03-30 20:14:59 +0900

    add

---
+++ ExaWizards2019/A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <queue>
+//greaterで必要
+#include <functional>
+//重複チェック
+#include <set>
+
+
+using namespace std;
+typedef long long ll;
+ll N;
+ll M;
+ll r;
+ll INF = 1<<28;
+
+
+int main() {
+    //頂点数
+    cin >> N >> M >> r;
+    vector<vector<pair<ll, ll> > > G(N);
+
+    ll a, b, t;
+    for(ll i = 0; i < M; i++){   
+        cin >> a >> b >> t;
+
+    }
+
+
+    
+}
