04a79ac39fb28d290f5de810f5344716f8999019
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-04-14 01:05:00 +0900

    うおお

--- ABC124/04.cpp
+++ ABC124/04.cpp
@@ -12,33 +12,30 @@
 
 using namespace std;
 typedef long long ll;
-ll N;
 
 int main(){
-    //int N, K;
-    //cin >> N >> K;
+    int N, K;
+    cin >> N >> K;
     string s;
     cin >> s;
-
+    vector<int > vec;
+    //一番左端が0なら調整を加える
     bool swi;
     if(s.substr(0, 1) == "0"){
         swi = 0;
     }else{
         swi = 1;
     }
-
-    vector<int > ans;
-
     if(swi == 0){
-        ans.push_back(0);
+        vec.push_back(0);
     }
-
+    //０または１が連続するところをまとめる処理
     int f = 0;
     int cont = 1;
     for(int i = 0; i < s.length()-1; i++){
         if(swi == 0){
             if(s.substr(i, 1) == "0" && s.substr(i+1, 1) == "1"){
-                ans.push_back(cont);
+                vec.push_back(cont);
                 swi = 1;
                 cont = 1;
             }else{
@@ -46,7 +43,7 @@ int main(){
             }
         }else{
             if(s.substr(i, 1) == "1" && s.substr(i+1, 1) == "0"){
-                ans.push_back(cont);
+                vec.push_back(cont);
                 swi = 0;
                 cont = 1;
             }else{
@@ -55,29 +52,49 @@ int main(){
         }
 
     }
-    ans.push_back(cont);
+    vec.push_back(cont);
+
     //数合わせ
+    /*
     if(ans.size() % 2 != 0){
         ans.push_back(0);
     }
+    */
 
-    int ff = 0;
-    for(int i = 0; i < 1; i++){
-        if(ff == 0){
-            vector<int > temp;
-            for(int j = 0; j < ans.size(); j += 2){
-                cout << ans[j] + ans[j+1] + ans[j+2] << endl;
-                temp.push_back(ans[j] + ans[j+1] + ans[j+2]);
-            }
-            for(int j = 0; j < temp.size(); j++){
-                cout << temp[j] << endl;
-            }
-        } 
+    //確認
+    /*
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec[i];
     }
-    for(int i = 0; i < ans.size(); i++){
+    cout << endl;
+    */
 
-        
+    int range = 2 * K + 1;
+    int left = 0;
+    int right = 0;
+    int temp = 0;
+    int ans = 0;
+    //しゃくとり方
+    for(int i = 0; i < vec.size(); i += 2){
+        int Nextleft = i;
+        //右端を貫通しないように制限を加える
+        int Nextright = min(i + range, (int)vec.size());
+        //前の左端から今回の左端まで（差分）
+        while(Nextleft > left){
+            temp -= vec[left];
+            left++;
+        }
+        //前の右端から今回の右端まで（差分）
+        while(Nextright > right){
+            temp += vec[right];
+            right++;
+        }
+
+        ans = max(ans, temp);
+
     }
+
+    cout << ans << endl;
 
 
 } 
