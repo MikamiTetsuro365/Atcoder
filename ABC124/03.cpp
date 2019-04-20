f8faf40c89fdda3a77e056c37a34d961caa65096
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-04-20 23:34:04 +0900

    add

--- ABC124/03.cpp
+++ ABC124/03.cpp
@@ -12,89 +12,54 @@
 
 using namespace std;
 typedef long long ll;
+ll N;
 
 int main(){
-    int N, K;
-    cin >> N >> K;
     string s;
     cin >> s;
-    vector<int > vec;
-    //一番左端が0なら調整を加える
-    bool swi;
-    if(s.substr(0, 1) == "0"){
-        swi = 0;
-    }else{
-        swi = 1;
-    }
-    if(swi == 0){
-        vec.push_back(0);
-    }
-    //０または１が連続するところをまとめる処理
-    int f = 0;
-    int cont = 1;
-    for(int i = 0; i < s.length()-1; i++){
-        if(swi == 0){
-            if(s.substr(i, 1) == "0" && s.substr(i+1, 1) == "1"){
-                vec.push_back(cont);
-                swi = 1;
-                cont = 1;
-            }else{
-                cont++;
-            }
+    int N = s.length();
+
+    vector<int > t_s(N, 0);
+    vector<int > p1(N, 0);
+    vector<int > p2(N, 0);
+
+    for(int i = 1; i <= N; i++){
+        if(s.substr(i-1,1) == "0"){
+            t_s[i-1] = 0;
         }else{
-            if(s.substr(i, 1) == "1" && s.substr(i+1, 1) == "0"){
-                vec.push_back(cont);
-                swi = 0;
-                cont = 1;
-            }else{
-                cont++;
-            }
+            t_s[i-1] = 1;
+        }  
+
+        if(i % 2 == 0){
+            p1[i-1] = 1;
+            p2[i-1] = 0;
+        }else{
+            p1[i-1] = 0;
+            p2[i-1] = 1;            
+        }  
+    }
+
+    int ans1 = 0;
+    int ans2 = 0;
+    for(int i = 0; i < N; i++){
+        if(t_s[i] == 0 && p1[i] == 1){
+            ans1++;
         }
-
-    }
-    vec.push_back(cont);
-
-    //数合わせ
-    /*
-    if(ans.size() % 2 != 0){
-        ans.push_back(0);
-    }
-    */
-
-    //確認
-    /*
-    for(int i = 0; i < vec.size(); i++){
-        cout << vec[i];
-    }
-    cout << endl;
-    */
-
-    int range = 2 * K + 1;
-    int left = 0;
-    int right = 0;
-    int temp = 0;
-    int ans = 0;
-    //しゃくとり方
-    for(int i = 0; i < vec.size(); i += 2){
-        int Nextleft = i;
-        //右端を貫通しないように制限を加える
-        int Nextright = min(i + range, (int)vec.size());
-        //前の左端から今回の左端まで（差分）引く
-        while(Nextleft > left){
-            temp -= vec[left];
-            left++;
+        if(t_s[i] == 1 && p1[i] == 0){
+            ans1++;
         }
-        //前の右端から今回の右端まで（差分）足す
-        while(Nextright > right){
-            temp += vec[right];
-            right++;
+        if(t_s[i] == 0 && p2[i] == 1){
+            ans2++;
         }
-
-        ans = max(ans, temp);
-
+                if(t_s[i] == 0 && p2[i] == 1){
+            ans2++;
+        }
     }
-
-    cout << ans << endl;
-
+    
+    if(ans1 > ans2){
+        cout << ans2 << endl;
+    }else{
+        cout << ans1 << endl;
+    }
 
 } 
