e86136ccdf9831c6d9e2f0c39557914dda08cc23
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-07-01 17:59:29 +0900

    first commit

---
+++ ABC105/04.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <istream>
+#include <iomanip>
+#include <typeinfo>
+#include <stack>
+#include <map>
+
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ll N, M;
+    cin >> N >> M;
+    vector<ll > A(N);
+
+    //vector<ll >
+    
+    ll sum = 0;
+    for(ll i = 0; i < N; i++){
+        cin >> A[i];
+    }
+
+    //累積和
+    vector<ll > ruiseki;
+    ruiseki.push_back(0);
+    for(ll i = 0; i < N; i++){
+        sum += A[i];
+        ruiseki.push_back(sum);
+    }   
+    //cout << ruiseki.size() << endl;
+    //累積わにMOD
+    ll ans = 0;
+    //連想配列用意
+    map<ll, ll> result;
+    for(ll i = 0; i < ruiseki.size(); i++){
+        //ruiseki[i] %= M;
+        //cout << ruiseki[i] << " ";
+        result[ruiseki[i] % M]++;
+    }     
+    //cout << endl;
+    for(auto i = result.begin(); i != result.end() ; ++i){
+        ll value = i->second;
+        if(value == 1) continue;
+        ans += value * (value - 1) / 2;
+    }
+    //ans = ans * (ans - 1) / 2;
+    cout << ans << endl;
+
+
+} 
--- ABC130/03.cpp
+++ ABC130/03.cpp
@@ -24,54 +24,14 @@ int main(){
     //cin >> N;
     double W, H, x , y;
     cin >> W >> H >> x >> y;
-    double sum = 0;
-    //cout << sum << endl;
-    if(x == 0){
-        if(y == 0){
-            sum = W * H / 2.0;
-            cout << sum << " " << "0" << endl;
-            return 0;
-        }
-        sum = 0.000000001 * (H - y) / 2;
-        cout << sum << " " << "1" << endl;
-        return 0;
-    }else if(y == 0){
-        if(x == 0){
-            sum = W * H / 2.0;
-            cout << sum << " " << "0" << endl;
-            return 0;
-        }
-        sum = 0.000000001 * (W - x) / 2;
-        cout << sum << " " << "1" << endl;
-        return 0;
-    }
-
     
-
-    if(W == H){
-
-        if(x == y){
-            sum = W * H / 2.0;
-            cout << sum << " " << "1" << endl;
-        }else{
-            if(x < y){
-                sum = x * H * 1.0;
-                cout << sum  << " " << "0" << endl;
-            }else{
-                sum = y * W * 1.0;
-                cout << sum  << " " << "0" << endl;
-            }
-        }
+    if(W / 2 == x && H / 2 == y){
+        cout << setprecision(9)  << W * H / 2.0;
+        cout << " 1"  << endl;
     }else{
-            if(x < y){
-                sum = x * H * 1.0;
-                cout << sum  << " " << "0" << endl;
-            }else{
-                sum = y * W * 1.0;
-                cout << sum  << " " << "0" << endl;
-            }      
+        cout << setprecision(9)  << W * H / 2.0;
+        cout << " 0"  << endl;
     }
-
 
 
 } 
