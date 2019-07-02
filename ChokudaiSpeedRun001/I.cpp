c1636e6dee457d24994761bdde5585a70e751be1
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-07-02 11:27:39 +0900

    first commit

--- ChokudaiSpeedRun001/I.cpp
+++ ChokudaiSpeedRun001/I.cpp
@@ -19,32 +19,36 @@ int main(){
     cin >> N;
     vector<ll > A(N);
     //入力
-    ll sum = 0;
     for(ll i = 0; i < N; i++){
         cin >> A[i];
     }  
     //累積和
+    ll sum = 0;
     vector<ll > ruiseki;
     ruiseki.push_back(0);
     for(ll i = 0; i < N; i++){
         sum += A[i];
         ruiseki.push_back(sum);
     }   
-    //累積わにMOD
+
+    ll r = 1;
+    ll l = 0;
+    sum = ruiseki[1];
     ll ans = 0;
-    //連想配列用意
-    map<ll, ll> result;
-    
-    for(ll i = 0; i < ruiseki.size(); i++){
-        result[ruiseki[i] % N]++;
-        cout << ruiseki[i] % N;
-    }     
-    cout << endl;
-    
-    for(auto i = result.begin(); i != result.end() ; ++i){
-        ll value = i->second;
-        if(value == 1) continue;
-        ans += value * (value - 1) / 2;
+    //しゃくとり法
+    while(r <= N && l <= N ){
+        //cout << l << ":" << r << ":" << sum << endl;
+        if(sum == N){
+            ans++;
+            l++;
+        }
+        if(sum > N){
+            l++;
+        }
+        if(sum < N){
+            r++;
+        }
+        sum = ruiseki[r] - ruiseki[l];
     }
 
     cout << ans << endl;
