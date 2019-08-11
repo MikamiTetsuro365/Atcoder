eeab1424de36c1f9075e9570518aee11f17deed8
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-08-11 10:45:29 +0900

    1919

--- ABC135/D.cpp
+++ ABC135/D.cpp
@@ -13,107 +13,37 @@
  
 using namespace std;
 typedef long long int ll;
-ll f_flg[1145141];
-ll s_flg[1145141];
-vector<ll > G[1145141];
-//各スタート位置から対戦相手のノードに到るまで繋がっているノードの総数
-ll Fennec = -1;
-ll Snuke = -1;
-//FennecがSnukeまでたどり着くのに到達するまでにどの頂点を通ったか？とスターチ位置からの距離を格納するやつ
-vector<pair <ll, ll > > where(1145141);
-//取り合うことで勝負が決まる　ノードとノードで挟まれた回廊
-ll battleZone[1145141];
-ll N = 0;
-ll ans = 0;
-//flg = 1 すでに到達済み 0＝まだ到達していない
-//player == 0 Fennec, player == 1 sunuke
 
-ll dfs(ll idx, ll end , ll from = -1){
-    
-    //どこからどこへ行く？
-    if(idx != 1) where[idx] = make_pair(from, battleZone[from]);  
-
-    if(idx == end){
-        //伝搬させておくと楽
-        battleZone[idx] = battleZone[from];
-        return 0;
-    }
-
-    if(from != -1){
-        //はじめ以外伝搬
-        battleZone[idx] = battleZone[from] + 1;
-    }  
-
-    if(f_flg[idx] == 1){
-        return 0;
-    }
-    f_flg[idx] = 1;
-
-    //cout << from << endl;
-    for(ll i = 0; i < G[idx].size(); i++){
-        //次の行き先は？
-        ll to = G[idx][i];
-
-        //同じだったらもう処理しない
-        if(to == from){
-            continue;
-        }
-        dfs(to, end, idx);
-        ans++;
-    }
-    
-    return 0;
-}
+ll MOD = 1000000007;
  
 int main(){
- 
-    cin >> N;
-    vector<ll > u(N-1);
-    vector<ll > v(N-1);
- 
-    for(ll i = 0; i < N-1; i++){
-        cin >> u[i];
-        cin >> v[i];
-        G[u[i]].push_back(v[i]);
-        G[v[i]].push_back(u[i]);
-    }
- 
-    battleZone[0] = 0;
-    battleZone[1] = 0;
-    //Fennecが塗りつぶせる数 スタート：ノード１
-    dfs(1, N);
-    
-    ll j = N;
-    ll battaleZoneDistance = 0;
-    vector<ll > battleZoneNode;
 
-    while(j != 1){
-        if(where[j].first != 0){
-            //cout << where[j].first << endl;
-            battleZoneNode.insert(battleZoneNode.begin(), where[j].first);
-            j = where[j].first;
-            battaleZoneDistance++;
+    string S;
+    cin >> S;
+
+    vector<vector<ll > > dp(S.length() + 1, vector<ll >(13, 0));
+
+    dp[0][0] = 1;
+
+    for(ll i = 0; i < S.length(); i++){
+
+        //0~9までを入れたとき前からのMODがどう変化するか
+        for(ll j = 0; j < 10; j++){
+            //？ではなく数字が書かれているときは数字分の遷移しか考えない
+            if(S[i] != '?' && (S[i] - '0') != j ){
+                continue;
+            }
+            for(ll h = 0; h < 13; h++){
+                dp[i + 1][(h * 10 + j) % 13] += dp[i][h];
+            }
         }
-        if(where[j].first == 1){
-            break;
+
+        //答えが超えないように割っておく
+        for(ll j = 0; j < 13; j++){
+            dp[i + 1][j] %= MOD;
         }
     }
-
-    ll trueGoal = battleZoneNode[where[N].second / 2 + where[N].second % 2 ];
-    //フラグ初期化
-    for(ll i = 0; i < 1145141; i++){
-        f_flg[i] = 0;
-    }
-    //cout << ans << endl;
-    ans = 0;
-    dfs(1,trueGoal);
-    //cout << ans << "," << trueGoal << endl;
-
-    if(N - ans - 1 < ans - 1){
-        cout << "Fennec" << endl;
-    }else{
-        cout << "Snuke" << endl;
-    }
  
- 
+    cout << dp[S.length()][5] << endl;
+
 } 
