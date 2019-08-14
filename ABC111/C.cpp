e0044375ee30abfa80baf7143b840f161d93fc6c
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-08-14 11:48:50 +0900

    1919

--- ABC111/C.cpp
+++ ABC111/C.cpp
@@ -29,57 +29,36 @@ ll search_v(vector<ll > v, ll in){
 int main(){
     //奇数と偶数番号で別に考える
     cin >> N;
-    //map<ll, ll> k_mp;
-    //map<ll, ll> g_mp;
-    vector<ll > k_v;
-    vector<ll > k_v_sum;
-    vector<ll > g_v;
-    vector<ll > g_v_sum;
+    map<ll, ll> k_mp;
+    map<ll, ll> g_mp;
+
+    ll g_num = 0;
+    ll g_max = 0;
+
+    ll k_num = 0;
+    ll k_max = 0;
 
     for(ll i = 0; i < N; i++){
         ll in;
-        ll st;
         cin >> in;
         if(i % 2 == 0){
-            st = search_v(g_v, in);
-            //cout << st << endl;
-            if(st == -1){
-                g_v.push_back(in);
-                g_v_sum.push_back(1);
-            }else{
-                g_v_sum[st]++;
+            g_mp[in]++;
+
+            if(g_max < g_mp[in]){
+                g_num = in;
+                g_max = g_mp[in];
             }
 
         }else{
-            st = search_v(k_v, in);
-            //cout << st << endl;
-            if(st == -1){
-                k_v.push_back(in);
-                k_v_sum.push_back(1);
-            }else{
-                k_v_sum[st]++;
+            k_mp[in]++;
+            if(k_max < k_mp[in]){
+                k_num = in;
+                k_max = k_mp[in];
             }
         }
     }
 
-    sort(g_v_sum.begin(), g_v_sum.end());
-    sort(k_v_sum.begin(), k_v_sum.end());
-
-    //答えだす
-    ll ans = 0;
-
-    for(ll i = 0; i < g_v_sum.size()-1; i++){
-        if(g_v_sum.size() == 1){
-            break;
-        }
-        ans += g_v_sum[i];
-    }
-    for(ll i = 0; i < k_v_sum.size()-1; i++){
-        if(k_v_sum.size() == 1){
-            break;
-        }
-        ans += k_v_sum[i];
-    }
+    //ll ans = N / 2;
 
     cout << ans << endl;
 }
--- ABC137/D.cpp
+++ ABC137/D.cpp
@@ -10,13 +10,47 @@
 #include <typeinfo>
 #include <stack>
 #include <map> 
+#include <queue>
 
 using namespace std;
 typedef long long ll;
-ll N;
+ll N, M;
 
 int main(){
 
+    ll ans = 0;
+
+    cin >> N >> M;
+
+    map<ll, vector<ll > > v;
+
+    for(ll i = 0; i < N; i++){
+        ll tempA, tempB;
+        cin >> tempA >> tempB;
+        v[tempA].push_back(tempB);
+    }
+
+    priority_queue<ll > q;
+
+    //M日分しかループは回さない
+    //M日後から1日, 2日・・・i日前を考えていく
+    for(ll i = 1; i <= M; i++){
+        //i日前で候補があるか調べる
+        for(ll j = 0; j < v[i].size(); j++){
+            //atでキーに対応する値を返す
+            q.push(v[i].at(j));
+        }
+
+        if(q.size() == 0){
+            continue;
+        }
+
+        ans += q.top();
+        q.pop();
+
+    }
+
+    cout << ans << endl;
 
 
 }
