10406aa8253ddc7e1976a1e4209e657fcb4c6d70
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-10-07 02:00:41 +0900

    kuso

--- JOI/2010/C.cpp
+++ JOI/2010/C.cpp
@@ -8,31 +8,48 @@ int main(){
     ll N, M;
     cin >> N >> M;
     
-    //初期化
-    G.assign(N, vector<pair<ll, ll > >());
-    //ans.assign(N, 0);
-    //vector<vector<pair<ll, ll > > > newG;
+    vector<vector<ll > > vec(M , vector<ll >(2, 0));
  
     //グラフ入力
     for(ll i = 0; i < M; i++){
-        ll u, v, w;
-        w = 0;
-        cin >> u >> v;
-        u--;
-        v--;
-        //双方向に貼りましょうね
-        G[u].push_back(make_pair(v, w));
-        G[v].push_back(make_pair(u, w));
+        ll a, b; cin >> a >> b;
+        vec[i][0] = a;
+        vec[i][1] = b;
     }
 
-    //友達はいない
-    if(G[0].size() == 0){
-        cout << 0 << endl;
-        return 0;
-    }
+    vector<vector<ll > > ans(2 , vector<ll >(N + 1, 0));
 
-    seen.assign(N, false); 
-    dfs(0);
+    //友達
+    for(ll i = 0; i < M; i++){
+        if(vec[i][0] == 1){
+            ans[0][vec[i][1]] = 1;
+        }else if(vec[i][1] == 1){
+            ans[0][vec[i][0]] = 1;
+        }
+    } 
+    //友達の友達
+    for(ll i = 0; i < N + 1; i++){
+        if(ans[0][i] == 1){
+            for(ll j = 0; j < M; j++){
+                if(vec[j][0] == i){
+                    if(ans[0][vec[j][1]] == 1) continue;
+                    ans[1][vec[j][1]] = 1;
+                }else if(vec[j][1] == i){
+                    if(ans[0][vec[j][0]] == 1) continue;
+                    ans[1][vec[j][0]] = 1;
+                }
+            }
+        }
+    }     
 
-    cout << ans << endl;
+    ll friend_num = 0;
+    for(ll i = 0; i < 2; i++){
+        for(ll j = 2; j < N + 1; j++){
+            if(ans[i][j] == 1){
+                friend_num++;
+            }
+        }   
+    }     
+    cout << friend_num << endl;
+
 }
