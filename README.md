28f9bc5c1e534e45570f092de64a1eabb0f54224
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-10-20 20:32:11 +0900

    1919

--- README.md
+++ README.md
@@ -551,6 +551,83 @@ int main() {
 ```
 
 # ベルマンフォワード法
+**負の辺があってもよい．負の閉路が検出できる．**
+```cpp
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll > pi;  
+typedef pair<pair<ll, ll >, ll > pii;  
+vector<ll > vec;
+vector<vector<ll > > vec2;
+ll MOD = 1000000007;
+ll INF = 114514191945;
+ll N, M; 
+vector<vector<pi > > G;
+vector<ll > dist;
+
+//ベルマンフォード
+bool bellman(ll start){
+    //準備
+    //vector<ll > dist(N, INF);
+    //初期化
+    dist[start] = 0;
+    
+    for(ll i = 0; i < N; i++){
+        for(ll from = 0; from < N; from++){
+            for(ll j = 0; j < G[from].size(); j++){
+                ll to = G[from][j].first;
+                ll cost = G[from][j].second;
+                if(dist[from] != INF && dist[to] > dist[from] + cost){
+                    dist[to] = dist[from] + cost;
+                    if(i == N-1){
+                        //負の経路が会って頂点の数以上にループが回ってしまう
+                        return false;
+                    }
+                }
+            }       
+        }
+    }
+
+    //確認
+    /*
+    for(ll i = 0; i < N; i++){
+        if(dist[i] == INF){
+            cout << "INF" << endl;
+        }else{
+            cout << dist[i] << endl;
+        }
+    }
+    */
+
+   return true;
+
+}
+
+int main() {
+    //探査開始点
+    ll S;
+    cin >> N >> M >> S;
+
+    G.assign(N, vector<pi >());
+    dist.assign(N, INF);
+
+    for(ll i = 0; i < M; i++){   
+        ll u, v, w;
+        cin >> u >> v >> w;
+        //u--;
+        //v--;
+        //双方向に貼りましょうね
+        G[u].push_back(make_pair(v, w));
+        //G[v].push_back(make_pair(u, w)); 
+    }
+    
+    if(bellman(S) == false){
+        cout << "NEGATIVE CYCLE" << endl;
+    }
+}
+```
 
 # ワーシャルフロイド法
 **あらかじめ全ての頂点間の最短距離を求めておく 計算量O(V^3) 負の辺があってもOK**
--- other/Bellman–Ford/BellmanFord.cpp
+++ other/Bellman–Ford/BellmanFord.cpp
@@ -1,48 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <string>
-#include <cmath>
-#include <iomanip>
-#include <queue>
-//greaterで必要
-#include <functional>
-//重複チェック
-#include <set>
-
+#include "bits/stdc++.h"
 
 using namespace std;
-typedef long long ll;
-ll N;
-ll M;
-ll r;
-ll INF = 1<<28;
-//ベルマンフォード
-bool bellman(vector<vector<pair<ll, ll> > > G, ll start){
-    //準備
-    vector<ll > dist(N, 1<<28);
+typedef long long int ll;
+typedef pair<ll, ll > pi;  
+typedef pair<pair<ll, ll >, ll > pii;  
+vector<ll > vec;
+vector<vector<ll > > vec2;
+ll MOD = 1000000007;
+ll INF = 114514191945;
+ll N, M; 
+vector<vector<pi > > G;
+vector<ll > dist;
 
+//ベルマンフォード
+bool bellman(ll start){
+    //準備
+    //vector<ll > dist(N, INF);
     //初期化
     dist[start] = 0;
     
     for(ll i = 0; i < N; i++){
         for(ll from = 0; from < N; from++){
             for(ll j = 0; j < G[from].size(); j++){
-                ll cost = G[from][j].first;
-                ll to = G[from][j].second;
+                ll to = G[from][j].first;
+                ll cost = G[from][j].second;
                 if(dist[from] != INF && dist[to] > dist[from] + cost){
                     dist[to] = dist[from] + cost;
-                }
-                if(i == N-1){
-                    //負の経路が会って頂点の数以上にループが回ってしまう
-                    return false;
+                    if(i == N-1){
+                        //負の経路が会って頂点の数以上にループが回ってしまう
+                        return false;
+                    }
                 }
             }       
         }
     }
 
     //確認
+    /*
     for(ll i = 0; i < N; i++){
         if(dist[i] == INF){
             cout << "INF" << endl;
@@ -50,31 +44,31 @@ bool bellman(vector<vector<pair<ll, ll> > > G, ll start){
             cout << dist[i] << endl;
         }
     }
-
+    */
 
    return true;
 
 }
 
 int main() {
-    //頂点数
-    cin >> N >> M >> r;
-    vector<vector<pair<ll, ll> > > G(N);
+    //探査開始点
+    ll S;
+    cin >> N >> M >> S;
 
-    ll a, b, t;
+    G.assign(N, vector<pi >());
+    dist.assign(N, INF);
+
     for(ll i = 0; i < M; i++){   
-        cin >> a >> b >> t;
-        //入力の仕方によってはマイナスしとく
-        //a--;
-        //b--;
-        //無効グラフなので
-        G[a].push_back(make_pair(t,b));
-    }
-
-    bool minus = bellman(G, r);
-    
-    if(minus == false){
-        cout << "loop" << endl;
+        ll u, v, w;
+        cin >> u >> v >> w;
+        //u--;
+        //v--;
+        //双方向に貼りましょうね
+        G[u].push_back(make_pair(v, w));
+        //G[v].push_back(make_pair(u, w)); 
     }
     
+    if(bellman(S) == false){
+        cout << "NEGATIVE CYCLE" << endl;
+    }
 }
