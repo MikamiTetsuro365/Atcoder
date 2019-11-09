1f052a54c2e5314189716da0f7bfbe6c93526a21
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-11-09 23:54:35 +0900

    1919

--- 20191109/A.cpp
+++ 20191109/A.cpp
@@ -13,7 +13,9 @@ int main(){
 
     ll N; cin >> N;
 
-    cout << "No" << endl;
-    
-
+    if(N % 2 == 0){
+        cout << N / 2 - 1 << endl;
+    }else{
+        cout << N / 2<< endl;
+    }
 }
