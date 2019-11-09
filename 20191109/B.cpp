d0dd201f7ca8c753fff33e5bb733dd9a303e04a7
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-11-09 23:44:06 +0900

    1919

--- 20191109/B.cpp
+++ 20191109/B.cpp
@@ -6,8 +6,20 @@ typedef pair<ll, ll > pi;
 typedef pair<pair<ll, ll >, ll > pii;  
 vector<ll > vec;
 vector<vector<ll > > vec2;
-ll MOD = 1000000007;
+ll MOD = 998244353;
 ll INF = 11451419194545;
+
+ll mod_pow(ll a, ll n){
+    if(n == 0) return 1;
+    //べき数nが奇数, aを前にだして, a^n-1の気持ちに
+    if(n % 2 == 1){
+        return a * fast_pow(a, n - 1) % MOD;
+    }else{
+        //べき数nが偶数のとき，べき数を半分にして, aをまとめる. べき数を半分にする
+        return fast_pow(a * a % MOD, n / 2) % MOD;
+    }
+}
+
 
 int main(){
     ll N;
@@ -20,18 +32,25 @@ int main(){
             cout << 0 << endl;
             return 0;
         }
-        if(i > 0){
-            mp[t]++;
-        }
-        
+        if(i != 0) mp[t]++;
     }
 
-    map<ll, ll> mp;
-    //ここになにか入力
+    ll cn = 1;
+    ll ans = 1;
+    ll tmp = 1;
     auto begin = mp.begin(), end = mp.end();
     for (auto iter = begin; iter != end; iter++) {
-        
+        if(iter -> first != cn){
+            cout << 0 << endl;
+            return 0;
+        }
+        cn++;
+        ans = ans * fast_pow(tmp, iter->second) % MOD;
+        //cout << iter->second << endl;
+        tmp = iter->second;
     }
+
+    cout << ans << endl;
 
 
 }
