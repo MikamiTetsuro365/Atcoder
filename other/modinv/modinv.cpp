732ae32397426b4b3d92170ad2169a53d27c1a4b
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-11-23 20:41:00 +0900

    1919

--- other/modinv/modinv.cpp
+++ other/modinv/modinv.cpp
@@ -1,60 +1,33 @@
-#include <iostream>
+#include "bits/stdc++.h"
 using namespace std;
+typedef long long int ll;
 
-long long extGCD(long long a, long long b, long long &x, long long &y) {
-    cout << a << " " << b  << endl;  
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
+pair<ll, ll > extgcd(ll a, ll b){
+    ll s, t, sx, sy, tx, ty;
+    s = a; t = b; sx = 1; sy = 0; tx = 0; ty = 1;
+
+    while(s % t != 0){
+        ll temp = s / t;
+        ll u = s - t * temp;
+        ll ux = sx - tx * temp;
+        ll uy = sy - ty * temp;
+
+        s = t;
+        sx = tx;
+        sy = ty;
+        t = u;
+        tx = ux;
+        ty = uy;
     }
-    
-    long long d = extGCD(b, a%b, y, x); // 再帰的に解く
-    cout << a << " " <<  b << " " <<  x << " " << y  << endl;  
-    y -= a / b * x;
-    
-    return d;
+
+    //逆元
+    return {tx, ty};
 }
 
-// mod. m での a の逆元 a^{-1} を計算する
-long long modinv(long long a, long long m) {
-    long long b = m, u = 1, v = 0;
-    while (b) {
-        long long t = a / b;
-        a -= t * b; swap(a, b);
-        u -= t * v; swap(u, v);
-    }
-    u %= m;
-    if (u < 0) u += m;
-    cout << u << " " << v << endl;
-    return u;
-}
-
-// a ÷ b mod. MOD を計算してみる
-int main() {
-
-    //割り算MOD
-    //割り算は掛け算で表せる
-    //つまり割る数の逆元を割られる数にかける
-    //
-
-    const int MOD = 1000000007;
-
-    long long a = 12345678900000;
-    long long b = 100000;
-
-    long long x, y;
-    extGCD(a, b, x, y);
-    cout << x << " " << y << endl;
-
-    // a を 10000000007 で割ってから b の逆元をかけて計算
-    a %= MOD;
-    cout << a * modinv(b, MOD) % MOD << endl;
-
-    /*
-    for (int i = 1; i < 13; ++i) {
-        cout << i << " 's inv: " << modinv(i, 13) << endl;
-    }
-    */
-
+int main(){
+    pair<ll, ll> xy;
+    ll a = 111;
+    ll b = 30;
+    xy = extgcd(a, b);
+    cout << xy.first << " " << xy.second << endl;
 }
