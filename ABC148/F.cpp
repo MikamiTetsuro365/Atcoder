0dccbea59b8459ad4bef1083558049a138011422
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-12-23 05:46:57 +0900

    1919

--- ABC148/F.cpp
+++ ABC148/F.cpp
@@ -6,69 +6,91 @@ ll N, M, L;
 ll ans = 0;
 
 ll prohibitedArea;
-ll mx;
+pair<ll, ll > mx;
 vector<bool > seen;
-vector<vector<pair<ll, ll> > > G;
-vector<ll > prever;
- 
-vector<ll > dijkstra(ll start){
-    //準備
-    vector<bool > used(N, false);
-    vector<ll > dist(N, 114514191919);
-    prever.assign(N, -1);
+//vector<vector<pair<ll, ll> > > G;
+//vector<ll > prever;
+//最短距離を求めて高橋くんが進出してしまうと勝負が終わってしまう頂点を探す．負けるには最低何手必要か？
+//勝負が終わってしまう頂点から青木くん側には絶対に進むことはできない．
+//なぜなら青木くん側に進んでしまうと勝負が終わってしまうタイミングが早まる→交互に移動しているはずなのに矛盾が生じるから
+//そこで，別の方向に最大限二人で動ける頂点を求める．
+//その頂点こそが最適に動いた時に高橋くんが追い詰められる頂点となる．つまりその頂点から後一歩でも動くと青木くんと遭遇して負ける．動かざるを得ない.
+//高橋くんが先手で青木くんが後手で青木くんの移動距離(手数)を求めたいので, マイナス1する．
+class dijkstra{
+    public:
+    ll N;
+    vector<vector<pair<ll, ll> > > G;
+    vector<ll > dist;
+    vector<bool > used;
+    vector<ll > prever;
     priority_queue<pair<ll, ll > , vector<pair<ll, ll > >, greater<pair<ll, ll > > > que;
-    //初期化
-    que.push(make_pair(0, start));
- 
-    //cout << que.top().first << endl;
- 
-    while(!que.empty()){
-        //距離
-        ll d = 0;
-        //次の頂点
-        ll t_v = 0;
-        d = que.top().first;
-        t_v = que.top().second;
-        //topから削除
-        que.pop();
-        //探査済みなら処理しない
-        if(used[t_v] == true) continue;
-        //探査済みにする
-        used[t_v] = true;
-        //キューの上には既に最小距離が来ているので
-        dist[t_v] = d;
-        
-        //次探査する頂点はt_vなのでt_vから更に次にの頂点の繋がりをキューにいれる
-        for(ll i = 0; i < G[t_v].size(); i++){
-            //もし探査済みの頂点で，そこまでの累積距離より今更新しようとしている距離が
-            //大きいときは更新しない
-            ll to = G[t_v][i].first;
-            ll cost = d+G[t_v][i].second;
-            if(dist[to] <= cost) continue;
-            prever[to] = t_v;
-            //累積距離と次の頂点
-            que.push(make_pair(cost, to));
-            //仮dist
-            dist[to] = cost;
-        }
-        //cout << endl;
-    }   
     
-    return dist;
- 
-}
 
-vector<ll > get_path(ll t){
-    vector<ll > path;
-    for(; t != -1; t = prever[t]){
-        //cout << prever[t] << endl;
-        path.push_back(t);
+    //グラフに頂点を追加と初期化諸々
+    dijkstra(ll n = 4545){
+        N = n;
+        G.assign(N, vector<pair<ll, ll > >());
+        dist.assign(N, 11451419191919);
+        used.assign(N, false);
+        prever.assign(N, -1);
     }
-    //reverse(path.begin(), path.end());
-    return path;
-}
+    //グラフに辺を追加
+    void addEdge(ll v, ll u, ll w = 1){
+        G[u].push_back(make_pair(v, w));
+    }
 
+    //最短距離計算(始点=s)
+    vector<ll > CalcDist(ll s){
+        que.push(make_pair(0, s));
+        while(!que.empty()){
+            //距離
+            ll d = 0;
+            //次の頂点
+            ll t_v = 0;
+            d = que.top().first;
+            t_v = que.top().second;
+            //topから削除
+            que.pop();
+            //探査済みなら処理しない
+            if(used[t_v] == true) continue;
+            //探査済みにする
+            used[t_v] = true;
+            //キューの上には既に最小距離が来ているので
+            dist[t_v] = d;
+            
+            //次探査する頂点はt_vなのでt_vから更に次にの頂点の繋がりをキューにいれる
+            for(ll i = 0; i < G[t_v].size(); i++){
+                //もし探査済みの頂点で，そこまでの累積距離より今更新しようとしている距離が
+                //大きいときは更新しない
+                ll to = G[t_v][i].first;
+                ll cost = d+G[t_v][i].second;
+                if(dist[to] <= cost) continue;
+                prever[to] = t_v;
+                //累積距離と次の頂点
+                que.push(make_pair(cost, to));
+                //仮dist
+                dist[to] = cost;
+            }
+        }   
+        return dist;
+    }
 
+    //経路列挙(終点=t)
+    vector<ll > get_path(ll t){
+        vector<ll > path;
+        //始点を追加
+        for(; t != -1; t = prever[t]){
+            //cout << prever[t] << endl;
+            path.push_back(t);
+        }
+        //reverse(path.begin(), path.end());
+        return path;
+    }
+
+};
+
+//ここは仮
+vector<vector<pair<ll, ll> > > dfsG;
 void dfs(ll idx, ll from = -1, ll pathSize = 0){
 
     seen[idx] = true;
@@ -77,11 +99,14 @@ void dfs(ll idx, ll from = -1, ll pathSize = 0){
         return;
     }
 
-    mx = max(mx, pathSize);
+    if(mx.first < pathSize){
+        mx.first = pathSize;
+        mx.second = idx;
+    }
 
-    for(ll i = 0; i < G[idx].size(); i++){
+    for(ll i = 0; i < dfsG[idx].size(); i++){
         //次の行き先は？
-        ll to = G[idx][i].first;
+        ll to = dfsG[idx][i].first;
         //探査済み
         if(seen[to] == true){
             continue;
@@ -96,40 +121,51 @@ int main() {
     //頂点数
     cin >> N >> M >> L;
 
-    G.assign(N, vector<pair<ll, ll > >());
-    seen.assign(N, false);
- 
+    //ダイクストラ構築
+    dijkstra D1(N);
+    dijkstra D2(N);
+    //dfsグラフ構築
+    dfsG.assign(N, vector<pair<ll, ll > >());
     for(ll i = 0; i < N-1; i++){   
-        ll u, v, w;
+        ll u, v;
         cin >> u >> v;
-        w = 1;
         u--;
         v--;
         //双方向に貼りましょうね
-        G[u].push_back(make_pair(v, w));
-        G[v].push_back(make_pair(u, w));    
+        D1.addEdge(u, v);
+        D1.addEdge(v, u);
+        //D2.addEdge(u, v);
+        //D2.addEdge(v, u);
+
+        dfsG[u].push_back(make_pair(v, 1));
+        dfsG[v].push_back(make_pair(u, 1));
     }
 
-    vector<ll > dist = dijkstra(M-1);
-    //頂点0からget_path(ある頂点)までの経路
-    vector<ll > path = get_path(L-1);
+    D1.CalcDist(M-1);
+    vector<ll > path = D1.get_path(L-1);
+    ll tmp = D1.dist[L-1] / 2 + D1.dist[L-1] % 2;
+    //ll next_start = 0;
+    //cout << path[tmp]  << " " << M - 1 << endl;
+    /*
+    for(ll i = 0; i < path.size(); i++){
+        cout << path[i] << " ";
+    }
+    cout << endl;
+    */
 
-    //cout << dist[L-1] << endl;
-    ll ans = dist[L-1] / 2 + dist[L-1] % 2;
-    //cout << ans << endl;
-    //cout << ans << endl;
-    //できるだけ近づいて後一歩で捕まえられるという位置
-    ll next_start = path[ans];
-    //cout << next_start << endl;
-    //後一歩で捕まえられるという位置には近づかずにどこまで逃げれるか
-    prohibitedArea = path[ans - 1];
-    //cout << prohibitedArea << endl;
-    //dfs 
-    
+    ll ans = tmp;
+
+    ll next_start = path[tmp];
+    prohibitedArea = path[tmp - 1];
+
+    //cout << next_start << " " << prohibitedArea << endl;
+
+    seen.assign(N, false);
     dfs(next_start);
+    //D2.CalcDist(L-1);
+    //ans += D2.dist[mx.second];
 
-    ans += mx;
-
-    cout << ans - 1 << endl;
+    ans += mx.first - 1;
+    cout << ans << endl;
 
 }
