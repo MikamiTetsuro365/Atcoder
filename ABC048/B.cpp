cfdfce5d9dd1b2b3afdd7895f94450109898cca3
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2020-01-09 23:59:36 +0900

    1919

---
+++ ABC048/B.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll > pi;  
+typedef pair<pair<ll, ll >, ll > pii;  
+vector<ll > vec;
+vector<vector<ll > > vec2;
+ll MOD = 1000000007;
+ll INF = 1145141919454519;
+
+int main() {
+    ll a, b, x;
+    cin >> a >> b >> x;
+
+    if(a == b){
+        //bが割り切れるなら1割り切れないなら0
+        if(b % x == 0){
+            cout << 1 << endl;
+            return 0;
+        }else if(b % x > 0){
+            cout << 0 << endl;
+            return 0;
+        }
+        
+        return 0;
+    }
+    
+    if(a <= 0){
+        cout << b / x + 1 << endl;
+    }else{
+        a--;
+        cout << (b / x) - (a / x) << endl;
+    }
+
+}
