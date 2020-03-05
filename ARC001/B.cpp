c2f6e9118b61aacf984dfd43620ed7f9fa329772
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2020-03-06 00:12:18 +0900

    1919

--- ARC001/B.cpp
+++ ARC001/B.cpp
@@ -12,36 +12,41 @@ ll INF = 1145141919454519;
 int main(){
 
     ll A, B;
-    cin >> A >> B;
-    A += 19;
-    B += 19;
+    cin >> A >> B;  
+
+    //余白
+    A += 100;
+    B += 100;
+
     vector<ll > O(4545, INF);
     O[A] = 0;
 
-    for(ll i = A; i <= B; i++){
-        O[i+1]  = min(O[i+1], O[i]+1);
-        O[i+5]  = min(O[i+5], O[i]+1);
-        O[i+10] = min(O[i+10], O[i]+1);
+    if(A <= B){
+        for(ll i = A; i <= B; i++){
+            O[i+1]  = min(O[i+1], O[i]+1);
+            O[i+5]  = min(O[i+5], O[i]+1);
+            O[i+10] = min(O[i+10], O[i]+1);
+        }
+
+        for(ll i = B+10; i >= B; i--){
+            O[i-1]  = min(O[i-1], O[i]+1);
+            O[i-5]  = min(O[i-5], O[i]+1);
+            O[i-10] = min(O[i-10], O[i]+1);
+        }
+    }else{
+        for(ll i = A; i >= B; i--){
+            O[i-1]  = min(O[i-1], O[i]+1);
+            O[i-5]  = min(O[i-5], O[i]+1);
+            O[i-10] = min(O[i-10], O[i]+1);
+        }
+
+        for(ll i = B-10; i <= B; i++){
+            O[i+1]  = min(O[i+1], O[i]+1);
+            O[i+5]  = min(O[i+5], O[i]+1);
+            O[i+10] = min(O[i+10], O[i]+1);
+        }        
     }
 
-    for(ll i = A; i <= B+10; i++){
-        cout << O[i] << " ";
-    }
-    cout << endl;
-    
+
     cout << O[B] << endl;
-
-    for(ll i = B+10; i >= B; i--){
-        O[i-1]  = min(O[i-1], O[i]+1);
-        O[i-5]  = min(O[i-5], O[i]+1);
-        O[i-10] = min(O[i-10], O[i]+1);
-    }
-
-    for(ll i = A; i <= B+10; i++){
-        cout << O[i] << " ";
-    }
-    cout << endl;
-
-    cout << O[B] << endl;
-
 }
