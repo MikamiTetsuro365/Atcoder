09979c800184b54f9e6ce7cac421a3ecedd10257
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2020-03-10 21:38:23 +0900

    1919

--- yukicoder/No979.cpp
+++ yukicoder/No979.cpp
@@ -9,6 +9,19 @@ vector<vector<ll > > vec2;
 ll MOD = 1000000007;
 ll INF = 1145141919454519;
 
+vector<ll > divisor(ll n) {
+  vector< ll > div;
+  for(ll i = 1; i * i <= n; i++) {
+    if(n % i == 0) {
+      div.push_back(i);
+      //重複許すマジ
+      if(i * i != n) div.push_back(n / i);
+    }
+  }
+  sort(div.begin(), div.end());
+  return (div);
+}
+
 int main() {
     ll N;
     cin >> N;
@@ -16,20 +29,23 @@ int main() {
         ll t; cin >> t;
         vec.push_back(t);
     }
-    //最長増加部分列
+    //最長増加部分列的な
     ll mx = 0;
-    vector<ll > dp(N, INF);
+    vector<ll > dp(N+19, 1);
+    //約数毎にどれだけ伸ばせるか？
+    vector<ll > len(19191919, 0);
     for(ll i = 0; i < N; i++){
-        //注目している数字以上になる最初の最小の箇所を見つける
-        //その方がのちのちに最長なりやすい
-        ll it = lower_bound(dp.begin(), dp.end(), vec[i]) - dp.begin();
-        if(i != 0){
-            
-        }else{
-            dp[it] = vec[i];
+        vector<ll > div = divisor(vec[i]);
+        for(ll j = 0; j < div.size()-1; j++){
+            //自分の約数方向に伸ばせる
+            dp[i] = max(len[div[j]]+1, dp[i]);
+            //cout << vec[i] << "::" << len[div[j]]+1 << endl;
         }
-        
-        mx = max(mx, it);
+        len[vec[i]] = max(dp[i], len[vec[i]]);
+        //cout << dp[i] << endl;
+        mx = max(dp[i], mx);
     }
+
+    cout << mx << endl;
 
 }
