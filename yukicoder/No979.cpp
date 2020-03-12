70cf4f9fbef186b89dc86d7ff4d937c530601d15
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2020-03-13 03:24:54 +0900

    1919

--- yukicoder/No979.cpp
+++ yukicoder/No979.cpp
@@ -29,34 +29,6 @@ int main() {
         ll t; cin >> t;
         vec.push_back(t);
     }
-<<<<<<< HEAD
-
-    //繋がっていけるのは約数の中身のみ
-    //約数ごとに長さを管理して，より長く繋げていけそうな奴を更新する
-    vector<ll > len(19191919, 0);
-
-    for(ll i = 0; i < N; i++){
-        
-    }
-
-
-
-    //最長増加部分列
-    // ll mx = 0;
-    // vector<ll > dp(N, INF);
-    // for(ll i = 0; i < N; i++){
-    //     //注目している数字以上になる最初の最小の箇所を見つける
-    //     //その方がのちのちに最長なりやすい
-    //     ll it = lower_bound(dp.begin(), dp.end(), vec[i]) - dp.begin();
-    //     if(i != 0){
-
-    //     }else{
-    //         dp[it] = vec[i];
-    //     }
-        
-    //     mx = max(mx, it);
-    // }
-=======
     //最長増加部分列的な
     ll mx = 0;
     vector<ll > dp(N+19, 1);
@@ -75,6 +47,5 @@ int main() {
     }
 
     cout << mx << endl;
->>>>>>> d853bd95fdc80a6cac65747414f2bc4fed970d53
 
 }
